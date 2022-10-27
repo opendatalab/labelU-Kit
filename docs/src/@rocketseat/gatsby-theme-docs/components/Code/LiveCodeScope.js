655cb9eb4b6186a9135c533e8de71c63f64e6d97
wuhui <wuhui@pjlab.org.cn>
2022-10-27 15:52:19 +0800

    fix: update docs

--- docs/src/@rocketseat/gatsby-theme-docs/components/Code/LiveCodeScope.js
+++ docs/src/@rocketseat/gatsby-theme-docs/components/Code/LiveCodeScope.js
@@ -3,7 +3,6 @@ import { mdx } from "@mdx-js/react";
 import React from "react";
 import AnnotationOperation from "@label-u/components";
 import "@label-u/components/dist/index.css";
-import "antd/dist/antd.min.css";
 
 export default {
   AnnotationOperation,
