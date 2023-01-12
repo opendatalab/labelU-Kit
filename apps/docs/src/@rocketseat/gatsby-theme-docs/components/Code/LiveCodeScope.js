1f43cdbf5ab3eaa962ad2fbbcbefe1c037daa1d0
关林 沈 <shenguanlin@pjlab.org.cn>
2023-01-12 18:25:40 +0800

    chore: 增加自动发布

---
+++ apps/docs/src/@rocketseat/gatsby-theme-docs/components/Code/LiveCodeScope.js
@@ -0,0 +1,13 @@
+import { css, jsx } from "@emotion/react";
+import { mdx } from "@mdx-js/react";
+import React from "react";
+import AnnotationOperation from "@label-u/components";
+import "@label-u/components/dist/index.css";
+
+export default {
+  AnnotationOperation,
+  css,
+  jsx,
+  mdx,
+  React,
+};
---
+++ apps/docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return "@label-u/components";
+}
