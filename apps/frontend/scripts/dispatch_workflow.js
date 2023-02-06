737535e512f859d68ec0f8b895190d0c5d74290c
gary-Shen <grinson50@gmail.com>
2023-02-06 14:48:37 +0800

    chore(frontend): add workflow dispatch

---
+++ apps/frontend/scripts/dispatch_workflow.js
@@ -0,0 +1,30 @@
+const minimist = require('minimist');
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN || process.env.GH_TOKEN,
+});
+function main() {
+  const args = minimist(process.argv.slice(2));
+  const [branch, nextVersion] = args._;
+  console.log(branch, nextVersion);
+  const url = `https://github.com/opendatalab/labelU-Kit/releases/download/${nextVersion}/frontend.zip`;
+  console.log('trigger labelu workflow', url);
+  octokit.actions.createWorkflowDispatch({
+    owner: 'opendatalab',
+    repo: 'labelU',
+    workflow_id: `main_cicd_pipeline.yml`,
+    ref: 'main',
+    inputs: {
+        version: nextVersion,
+        branch,
+        name: 'frontend',
+        assets_url: url,
+    }
+  }).then(() => {
+    console.log('trigger labelu workflow success');
+  }).catch(err => {
+    console.log('trigger labelu workflow failed', err);
+  });
+}
+
+main();
