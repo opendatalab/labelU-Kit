4c74132cffe12c970b5deba4a8320f41f9c4f721
gary-Shen <grinson50@gmail.com>
2023-02-07 15:40:20 +0800

    chore(workspace): add post-publish script to trigger robot message and create a pr

---
+++ scripts/post-publish.js
@@ -0,0 +1,56 @@
+const minimist = require('minimist');
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN || process.env.GH_TOKEN,
+});
+
+const wechatRobotUrl = process.env.WEBHOOK_URL;
+
+async function main() {
+  const args = minimist(process.argv.slice(2));
+  const [branchName, releaseNotes] = args._;
+
+  if (!wechatRobotUrl) {
+    console.log('wechat robot url is not set');
+    return;
+  }
+
+  if (!branchName) {
+    console.log('current branch is not set');
+    return;
+  }
+
+  fetch(wechatRobotUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      msgtype:"markdown",
+      markdown: {
+        content: releaseNotes
+      }
+    }),
+  }).then(() => {
+    console.log('send wechat robot success');
+  }).catch((err) => {
+    console.log('send wechat robot failed', err);
+  });
+
+  console.log('Create a pull request');
+
+  octokit.rest.pulls.create({
+    owner: 'opendatalab',
+    repo: 'labelU-Kit',
+    ref: branchName,
+    base: 'main',
+    body: releaseNotes,
+  }).then(() => {
+    console.log('Create a pull request success');
+  }).catch((err) => {
+    console.log('Create a pull request failed', err);
+  });
+}
+
+main();
