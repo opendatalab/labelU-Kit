c52d69c130808a7f123e141ff6e7b83331f86485
gary <gary-Shen@users.noreply.github.com>
2023-02-13 19:23:38 +0800

    Update apps/frontend/src/utils/scrollIntoEventTarget.ts

--- apps/frontend/src/utils/scrollIntoEventTarget.ts
+++ apps/frontend/src/utils/scrollIntoEventTarget.ts
@@ -41,7 +41,7 @@ export default function scrollIntoEventTarget(
   }
 
   const targetHeight = target.clientHeight;
-  // 元素距离页面底部的距离不变
+  // 元素距离页面底部的距离不变，target重新渲染了，在setTimeout会丢失其dom引用，所以在重新渲染前获取其距离。
   const distanceFromContainerBottom = wrapper.scrollHeight - getOffsetTopOfElement(target, wrapper as HTMLElement);
 
   setTimeout(() => {
