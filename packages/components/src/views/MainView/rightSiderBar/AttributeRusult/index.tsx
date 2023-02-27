2dce3b54174f6ca2dbf585f0571bb3cb44d7c296
wuhui1989 <751569801@qq.com>
2023-02-27 10:30:54 +0800

    Update packages/components/src/views/MainView/rightSiderBar/AttributeRusult/index.tsx
    Co-authored-by: gary <gary-Shen@users.noreply.github.com>

--- packages/components/src/views/MainView/rightSiderBar/AttributeRusult/index.tsx
+++ packages/components/src/views/MainView/rightSiderBar/AttributeRusult/index.tsx
@@ -352,7 +352,7 @@ const AttributeRusult: FC<IProps> = ({
                 item.order = item.order - 1;
               }
               res.push(item);
-            } else {
+            } else if (item.id) {
               if (item?.id) {
                 deleteBoxArray.push(item.id);
               }
