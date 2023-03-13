bebbec6cd0be023275293590e8aad29306da8a6f
gary-Shen <grinson50@gmail.com>
2023-03-13 12:45:14 +0800

    fix(frontend): config template json to ts files

---
+++ apps/frontend/src/pages/createTask/partials/annotationConfig/configs/pointTool.ts
@@ -0,0 +1,22 @@
+export default {
+  attribute: [
+    {
+      key: '',
+      value: '',
+    },
+  ],
+  tools: [
+    {
+      tool: 'pointTool',
+      config: {
+        isShowCursor: false,
+        attributeConfigurable: true,
+        copyBackwardResult: false,
+        textConfigurable: false,
+        textCheckType: 0,
+        customFormat: '',
+        attributeList: [{ key: '', value: '' }],
+      },
+    },
+  ],
+};
---
+++ apps/frontend/src/pages/createTask/partials/annotationConfig/configs/textTool.ts
@@ -0,0 +1,16 @@
+export default {
+  textConfig: [
+    {
+      label: '文本1',
+      key: 'text1',
+      required: true,
+      default: '',
+      maxLength: 200,
+    },
+  ],
+  tools: [
+    {
+      tool: 'textTool',
+    },
+  ],
+};
