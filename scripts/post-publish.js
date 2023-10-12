e38a6a4a855c329db86618f043e0d9a905077ed2
gary-Shen <grinson50@gmail.com>
2023-10-12 17:09:52 +0800

    update(workspace): update post script

--- scripts/post-publish.js
+++ scripts/post-publish.js
@@ -1,10 +1,5 @@
 const minimist = require('minimist');
-const { Octokit } = require('@octokit/rest');
 const nodeFetch = require('node-fetch');
-
-const octokit = new Octokit({
-  auth: process.env.GH_TOKEN,
-});
 
 function sendMessageToWechat(content) {
   const wechatRobotUrl = process.env.WEBHOOK_URL;
@@ -29,48 +24,12 @@ function sendMessageToWechat(content) {
   });
 }
 
-function createPullRequest({ branchName, body, title = branchName, base = 'main' }) {
-  if (!branchName) {
-    return Promise.reject('branch name is not set');
-  }
-
-  console.log('Create a pull request');
-
-  return octokit.rest.pulls
-    .create({
-      owner: 'opendatalab',
-      repo: 'labelU-Kit',
-      head: branchName,
-      title,
-      base,
-      body,
-    })
-    .then(() => {
-      console.log('Create a pull request success');
-    });
-}
-
 async function main() {
   const args = minimist(process.argv.slice(2));
-  const [branchName, releaseNotes] = args._;
+  const [, releaseNotes] = args._;
 
   try {
     await sendMessageToWechat(releaseNotes);
-
-    await new Promise((resolve) => {
-      setTimeout(async () => {
-        createPullRequest({
-          branchName,
-          body: releaseNotes,
-          base: 'main',
-          title: 'Update package version',
-        }).catch((err) => {
-          console.log(err);
-        });
-        resolve();
-        // 避免 You have exceeded a secondary rate limit 问题
-      }, 1000);
-    });
   } catch (err) {
     console.log(err);
   }
