8945918b599093369dba811b35c5ac23c12931b8
gary-Shen <grinson50@gmail.com>
2023-10-13 17:34:09 +0800

    fix(interface): update types and add typedoc

--- packages/interface/src/annotation/graphic/index.ts
+++ packages/interface/src/annotation/graphic/index.ts
@@ -1,1 +1,4 @@
+/**
+ * 2d图形标注类型，目前支持线、矩形、多边形、点
+ */
 export type GraphicAnnotationType = 'line' | 'rect' | 'polygon' | 'point';
--- packages/interface/src/annotation/video/index.ts
+++ packages/interface/src/annotation/video/index.ts
@@ -1,26 +1,55 @@
-import type { AttributeValue, LabelValue } from '../../base';
+import type { AudioSegmentAnnotation } from '../audio';
 
+/**
+ * 视频片断分割
+ *
+ * @description
+ * 与音频片断分割标注一致，组件层面其实与视频标注没有区别。
+ * 但是在业务层面，音频片断分割与音频断分割是两种不同的标注类型，因此在上层的业务层需要区分。
+ *
+ * @see {@link AudioSegmentName}
+ */
 export type VideoSegmentName = 'segment';
 
+/**
+ * 视频帧标注
+ *
+ * @description 与视频分割同理
+ *
+ * @see {@link VideoSegmentName}
+ * @see {@link AudioFrameName}
+ */
 export type VideoFrameName = 'frame';
 
+/**
+ * 视频的两种标注类型
+ */
 export type VideoAnnotationType = VideoSegmentName | VideoFrameName;
 
-export interface VideoSegmentAnnotation extends LabelValue {
-  type: VideoSegmentName;
-  id: string;
-  start: number;
-  end: number;
-  attributes?: AttributeValue;
-  order: number;
-}
+/**
+ * 视频片断分割标注数据
+ *
+ * @see {@link AudioSegmentName}
+ */
+export type VideoSegmentAnnotation = AudioSegmentAnnotation;
 
-export interface VideoFrameAnnotation extends LabelValue {
+/**
+ * 视频时间戳标注数据
+ */
+export interface VideoFrameAnnotation extends Omit<VideoSegmentAnnotation, 'start' | 'end' | 'type'> {
+  /**
+   * 时间戳标注类型
+   *
+   * @see {@link VideoFrameName}
+   */
   type: VideoFrameName;
-  id: string;
+  /**
+   * 时间戳
+   */
   time: number;
-  order: number;
-  attributes?: AttributeValue;
 }
 
+/**
+ * 视频标注数据
+ */
 export type VideoAnnotationData = VideoSegmentAnnotation | VideoFrameAnnotation;
