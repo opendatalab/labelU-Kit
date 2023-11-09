0d1a770ccc87b1f35652bd6a430ee92768e3beb1
gary-Shen <grinson50@gmail.com>
2023-11-09 16:12:05 +0800

    fix(components-react): fix attribute overlay

--- packages/components-react/vite.config.ts
+++ packages/components-react/vite.config.ts
@@ -2,14 +2,16 @@ import { resolve } from 'path';
 
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import svgr from 'vite-plugin-svgr';
+import dts from 'vite-plugin-dts';
 import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [peerDepsExternal() as PluginOption, tsconfigPaths(), svgr()],
+  plugins: [peerDepsExternal() as PluginOption, tsconfigPaths(), svgr(), dts({ rollupTypes: true })],
   build: {
+    target: 'esnext',
     lib: {
       entry: 'src/index.ts',
       name: 'VideoAnnotationReact',
