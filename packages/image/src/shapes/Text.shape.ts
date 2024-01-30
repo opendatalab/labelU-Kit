91c122b707da7dcdbb30a7aa8b91a25e86c48840
gary-Shen <grinson50@gmail.com>
2024-01-30 16:48:36 +0800

    fix(image): update text style

--- packages/image/src/shapes/Text.shape.ts
+++ packages/image/src/shapes/Text.shape.ts
@@ -55,13 +55,15 @@ export class ShapeText extends Shape<TextStyle> {
   public style: Required<TextStyle> = {
     fontSize: 14,
     fontFamily: 'Sans-serif',
-    stroke: 'rgba(0, 0, 0, 0.7)',
-    strokeWidth: 1,
+    stroke: 'transparent',
+    strokeWidth: 0,
     fill: DEFAULT_LABEL_COLOR,
     opacity: 1,
   };
 
   public text: string;
+
+  private _pixRatio = window.devicePixelRatio || 1;
 
   private _offscreenCanvas: HTMLCanvasElement = document.createElement('canvas');
 
@@ -96,19 +98,29 @@ export class ShapeText extends Shape<TextStyle> {
    * @description fillText 在Firefox上非常慢，使用离屏canvas渲染
    */
   private _renderTexts() {
-    const { text, _offscreenCanvas, style } = this;
+    const { text, _offscreenCanvas, style, _pixRatio } = this;
     const { fontSize, fontFamily, fill, stroke, strokeWidth } = style;
     const offCtx = _offscreenCanvas!.getContext('2d')!;
 
-    offCtx.font = `${fontSize}px ${fontFamily}`;
+    // 获取设备的像素比
+    _offscreenCanvas.style.width = `${_offscreenCanvas.width}px`;
+    _offscreenCanvas.style.height = `${_offscreenCanvas.height}px`;
+
+    // 调整 canvas 的大小以匹配设备的像素比
+    _offscreenCanvas!.width = _offscreenCanvas!.width * _pixRatio;
+    _offscreenCanvas!.height = _offscreenCanvas!.height * _pixRatio;
+
+    offCtx.font = `${fontSize * _pixRatio}px ${fontFamily}`;
     offCtx.fillStyle = fill;
     offCtx.strokeStyle = stroke;
+    offCtx.shadowColor = 'rgba(0, 0, 0, 0.8)';
+    offCtx.shadowBlur = 5;
     offCtx.lineWidth = strokeWidth;
 
     let line = '';
     let letters = '';
-    const textX = MARGIN;
-    let textY = MARGIN + fontSize;
+    const textX = MARGIN * _pixRatio;
+    let textY = (MARGIN + fontSize) * _pixRatio;
 
     const renderLine = (_line: string, x: number, y: number) => {
       offCtx.strokeText(_line, x, y);
@@ -120,13 +132,13 @@ export class ShapeText extends Shape<TextStyle> {
       if (text[n] === '\n') {
         renderLine(line, textX, textY);
         line = '';
-        textY += LINE_HEIGHT;
+        textY += LINE_HEIGHT * _pixRatio;
       } else {
         letters = line + text[n];
         if (offCtx.measureText(letters).width > MAX_WIDTH) {
           renderLine(line, textX, textY);
           line = text[n];
-          textY += LINE_HEIGHT;
+          textY += LINE_HEIGHT * _pixRatio;
         } else {
           line = letters;
         }
@@ -164,7 +176,7 @@ export class ShapeText extends Shape<TextStyle> {
   }
 
   public render(ctx: CanvasRenderingContext2D) {
-    const { dynamicCoordinate, style, _offscreenCanvas } = this;
+    const { dynamicCoordinate, style, _offscreenCanvas, _pixRatio } = this;
     const [{ x, y }] = dynamicCoordinate;
     const { opacity } = style;
 
@@ -172,7 +184,9 @@ export class ShapeText extends Shape<TextStyle> {
 
     ctx.globalAlpha = opacity!;
 
-    ctx.drawImage(_offscreenCanvas!, x, y);
+    if (_offscreenCanvas.width && _offscreenCanvas.height) {
+      ctx.drawImage(_offscreenCanvas!, x, y, _offscreenCanvas.width / _pixRatio, _offscreenCanvas.height / _pixRatio);
+    }
 
     // 恢复透明度
     ctx.globalAlpha = 1;
