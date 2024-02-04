a899a0a893146dbba53d5dbfa92017f94fa94f94
gary-Shen <grinson50@gmail.com>
2024-02-04 14:08:40 +0800

    fix(frontend): fix upload

--- apps/frontend/scripts/dispatch_workflow.js
+++ apps/frontend/scripts/dispatch_workflow.js
@@ -8,23 +8,11 @@ const octokit = new Octokit({
   auth: process.env.PERSONAL_TOKEN,
 });
 
-function findLatestVersion(versions) {
-  const maxVersionLength = Math.max(...versions.map((version) => version.split('.').length));
-  const versionNumbers = versions.map((version) => {
-    const versionParts = version.split('.');
-    return versionParts.reduce((total, part, index) => {
-      return total + parseInt(part.replace('release/v', '')) * Math.pow(10, (maxVersionLength - index - 1) * 3);
-    }, 0);
-  });
-
-  return versions[versionNumbers.indexOf(Math.max(...versionNumbers))];
-}
-
 function uploadAssets(version) {
   const file = fs.readFileSync(path.join(__dirname, '../frontend.zip'));
   const options = {
     hostname: 'static-files.shlab.tech',
-    path: `/upload/labelU-Kit/releases/download/v${version}`,
+    path: `/upload/labelU-Kit/releases/download/${version}`,
     method: 'POST',
     headers: {
       'Content-Type': 'application/zip',
@@ -45,6 +33,10 @@ function uploadAssets(version) {
         console.log('upload success!');
         resolve();
       });
+
+      if (res.statusCode && res.statusCode >= 400) {
+        reject(res);
+      }
     });
 
     req.write(file);
@@ -55,31 +47,9 @@ function uploadAssets(version) {
   });
 }
 
-function createPullRequest({ branchName, body, title = branchName, base = 'main' }) {
-  if (!branchName) {
-    return Promise.reject('branch name is not set');
-  }
-
-  console.log('Create a pull request');
-
-  return octokit.rest.pulls
-    .create({
-      owner: 'opendatalab',
-      repo: 'labelU-Kit',
-      head: branchName,
-      title,
-      base,
-      body,
-    })
-    .then(() => {
-      console.log('Create a pull request success');
-    });
-}
-
 function gitlabCiTrigger(nextVersion) {
-  const version = `v${nextVersion}`;
   // aliyun ecs 访问github有问题
-  const url = `https://static-files.shlab.tech/download/labelU-Kit/releases/download/${version}/frontend.zip`;
+  const url = `https://static-files.shlab.tech/download/labelU-Kit/releases/download/${nextVersion}/frontend.zip`;
   const gitlabTriggerUrl = new URL(
     `https://gitlab.shlab.tech/api/v4/projects/${process.env.GI_LABELU_PROJECT_ID}/trigger/pipeline?token=${process.env.GL_TRIGGER_TOKEN}&ref=test`,
   );
@@ -116,68 +86,12 @@ async function main() {
   const args = minimist(process.argv.slice(2));
   const [branch, nextVersion, releaseTime, releaseNotes] = args._;
   const version = `v${nextVersion}`;
-  const url = `https://github.com/opendatalab/labelU-Kit/releases/download/${version}/frontend.zip`;
 
   if (branch === 'online') {
     // 上传zip到静态服务器
-    await uploadAssets(nextVersion);
-    await gitlabCiTrigger(nextVersion);
-
-    return;
+    await uploadAssets(version);
+    await gitlabCiTrigger(version);
   }
-
-  const inputs = {
-    version: version,
-    branch,
-    name: 'frontend',
-    assets_url: url,
-    changelog: releaseNotes,
-  };
-
-  console.log('inputs', inputs);
-
-  const labelUBranches = await octokit.request('GET /repos/opendatalab/labelU/branches', {
-    owner: 'opendatalab',
-    repo: 'labelU',
-  });
-
-  const releaseBranches = (labelUBranches.data || [])
-    .filter((branch) => branch.name.startsWith('release/'))
-    .map((branch) => branch.name);
-  const latestReleaseVersion = findLatestVersion(releaseBranches);
-
-  console.log('labelu latest release version is', latestReleaseVersion);
-
-  octokit.actions
-    .createWorkflowDispatch({
-      owner: 'opendatalab',
-      repo: 'labelU',
-      workflow_id: `${branch === 'release' ? 'release_' : ''}cicd_pipeline.yml`,
-      ref: branch === 'release' ? latestReleaseVersion : 'dev',
-      inputs,
-    })
-    .then((res) => {
-      console.log(res);
-      console.log('trigger labelu workflow success');
-    })
-    .catch((err) => {
-      console.log('trigger labelu workflow failed', err);
-    });
-
-  await new Promise((resolve) => {
-    setTimeout(async () => {
-      createPullRequest({
-        branchName: branch,
-        body: releaseNotes,
-        base: 'main',
-        title: 'Update package version',
-      }).catch((err) => {
-        console.log(err);
-      });
-      resolve();
-      // 避免 You have exceeded a secondary rate limit 问题
-    }, 1000);
-  });
 }
 
 main();
