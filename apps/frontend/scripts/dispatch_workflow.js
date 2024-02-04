aaa911a39dd5773095804a62bb6c6bf61c09c2f9
gary-Shen <grinson50@gmail.com>
2024-02-04 11:30:54 +0800

    fix(frontend): update download url

--- apps/frontend/scripts/dispatch_workflow.js
+++ apps/frontend/scripts/dispatch_workflow.js
@@ -42,7 +42,7 @@ function createPullRequest({ branchName, body, title = branchName, base = 'main'
 function gitlabCiTrigger(nextVersion) {
   const version = `v${nextVersion}`;
   // aliyun ecs 访问github有问题
-  const url = `https://static-files.shlab.tech/upload/labelU-Kit/releases/download/${version}/frontend.zip`;
+  const url = `https://static-files.shlab.tech/download/labelU-Kit/releases/download/${version}/frontend.zip`;
   const gitlabTriggerUrl = new URL(
     `https://gitlab.shlab.tech/api/v4/projects/${process.env.GI_LABELU_PROJECT_ID}/trigger/pipeline?token=${process.env.GL_TRIGGER_TOKEN}&ref=test`,
   );
