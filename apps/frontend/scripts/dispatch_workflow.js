625a916424a869730bc790eea08023a3b3b3300e
gary-Shen <grinson50@gmail.com>
2024-02-04 14:17:09 +0800

    fix(frontend): fix upload

--- apps/frontend/scripts/dispatch_workflow.js
+++ apps/frontend/scripts/dispatch_workflow.js
@@ -1,51 +1,5 @@
 const minimist = require('minimist');
 const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const { Octokit } = require('@octokit/rest');
-
-const octokit = new Octokit({
-  auth: process.env.PERSONAL_TOKEN,
-});
-
-function uploadAssets(version) {
-  const file = fs.readFileSync(path.join(__dirname, '../frontend.zip'));
-  const options = {
-    hostname: 'static-files.shlab.tech',
-    path: `/upload/labelU-Kit/releases/download/${version}`,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/zip',
-      'Content-Length': file.length,
-      Authorization: `${process.env.STATIC_SERVER_TOKEN}`,
-    },
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      console.log(`statusCode: ${res.statusCode}`);
-
-      res.on('data', (d) => {
-        process.stdout.write(d);
-      });
-
-      res.on('end', () => {
-        console.log('upload success!');
-        resolve();
-      });
-
-      if (res.statusCode && res.statusCode >= 400) {
-        reject(res);
-      }
-    });
-
-    req.write(file);
-    req.end();
-    req.on('error', (e) => {
-      reject(e);
-    });
-  });
-}
 
 function gitlabCiTrigger(nextVersion) {
   // aliyun ecs 访问github有问题
@@ -88,8 +42,6 @@ async function main() {
   const version = `v${nextVersion}`;
 
   if (branch === 'online') {
-    // 上传zip到静态服务器
-    await uploadAssets(version);
     await gitlabCiTrigger(version);
   }
 }
