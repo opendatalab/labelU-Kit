39c8523df18299e0e4d2c09d39e2fafd1a97f614
gary-Shen <grinson50@gmail.com>
2024-03-20 17:18:12 +0800

    fix(image-annotator-react): fix global annotation onchange

--- packages/image-annotator-react/src/AttributePanel/index.tsx
+++ packages/image-annotator-react/src/AttributePanel/index.tsx
@@ -258,7 +258,12 @@ export function AttributePanel() {
   const titles = useMemo(() => {
     const _titles = [];
     // 将文本描述和标签分类合并成全局配置
-    if (globalToolConfig?.tag || globalToolConfig?.text || preLabelMapping.tag || preLabelMapping.text) {
+    if (
+      globalToolConfig?.tag ||
+      globalToolConfig?.text ||
+      Object.keys(preLabelMapping.tag ?? {}).length ||
+      Object.keys(preLabelMapping.text ?? {}).length
+    ) {
       let isCompleted = false;
 
       const globalAnnotationMapping: Record<string, TextAnnotationEntity | TagAnnotationEntity> = {};
