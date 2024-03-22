70cceb365eea385f74f0303e9a46be561e4be3d7
gary-Shen <grinson50@gmail.com>
2024-03-22 12:07:37 +0800

    docs(website): update website

--- apps/website/src/pages/video/index.tsx
+++ apps/website/src/pages/video/index.tsx
@@ -2,9 +2,9 @@ import { Annotator } from '@labelu/video-annotator-react';
 import type { VideoAnnotation, VideoAnnotationType } from '@labelu/video-react';
 import { useState } from 'react';
 
-const mockData1: VideoAnnotation[] = [
-  { id: 'b2tk865g3w', type: 'segment', start: 7.457498, end: 11.625751, order: 1, label: 'ship' },
-];
+const mockData1 = {
+  segment: [{ id: 'b2tk865g3w', type: 'segment', start: 7.457498, end: 11.625751, order: 1, label: 'ship' }],
+};
 
 const attributeData = [
   {
@@ -14,7 +14,7 @@ const attributeData = [
   },
 ];
 
-const mockData2: VideoAnnotation[] = [{ id: 'eb7wjsga4ei', type: 'frame', time: 12.00722, label: 'car', order: 1 }];
+const mockData2 = { frame: [{ id: 'eb7wjsga4ei', type: 'frame', time: 12.00722, label: 'car', order: 1 }] };
 
 export default function VideoPage() {
   const [editingType, setEditingType] = useState<VideoAnnotationType>('segment');
@@ -32,13 +32,13 @@ export default function VideoPage() {
       id: 1,
       name: 'video-segment',
       url: import.meta.env.BASE_URL + 'video-segment.mp4',
-      annotations: mockData1,
+      data: mockData1,
     },
     {
       id: 2,
       name: 'video-frame',
       url: import.meta.env.BASE_URL + 'video-frame.mp4',
-      annotations: mockData2,
+      data: mockData2,
     },
   ]);
 
