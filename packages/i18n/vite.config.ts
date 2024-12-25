217e456ba7cd25cede2fb4794c3092cd627a626d
gary-Shen <grinson50@gmail.com>
2024-12-25 15:07:54 +0800

    feat(i18n): add @labelu/i18n

--- packages/i18n/vite.config.ts
+++ packages/i18n/vite.config.ts
@@ -8,9 +8,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [peerDepsExternal() as PluginOption, tsconfigPaths()],
-  worker: {
-    format: 'iife',
-  },
   build: {
     lib: {
       entry: 'src/index.ts',
