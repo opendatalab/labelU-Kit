adcd4792cc8eea56696ba8611c58222f75d3fb2f
gary-Shen <grinson50@gmail.com>
2024-12-26 11:40:23 +0800

    fix(frontend): current language highlight

--- apps/frontend/src/components/LangSwitcher/index.tsx
+++ apps/frontend/src/components/LangSwitcher/index.tsx
@@ -5,25 +5,32 @@ import Icon from '@ant-design/icons';
 
 import { ReactComponent as I18nSvg } from '@/assets/svg/i18n.svg';
 
-const langOptions = [
-  {
-    key: 'zh-CN',
-    label: '简体中文',
-    value: 'zh-CN',
-  },
-  {
-    key: 'en-US',
-    label: 'English',
-    value: 'en-US',
-  },
-];
-
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation();
+
   const lang = useMemo(() => {
-    const _lang = ['zh', 'zh_CN', 'zh-CN'].includes(i18n.language) ? 'zh-CN' : 'en-US';
-    return langOptions.find((item) => item.key === _lang)?.label;
+    return ['zh', 'zh_CN', 'zh-CN'].includes(i18n.language) ? 'zh-CN' : 'en-US';
   }, [i18n.language]);
+
+  const langOptions = useMemo(
+    () => [
+      {
+        key: 'zh-CN',
+        label: '简体中文',
+        value: 'zh-CN',
+      },
+      {
+        key: 'en-US',
+        label: 'English',
+        value: 'en-US',
+      },
+    ],
+    [],
+  );
+
+  const langLabel = useMemo(() => {
+    return langOptions.find((item) => item.key === lang)?.label;
+  }, [lang, langOptions]);
 
   const changeLocale = (e: any) => {
     i18n.changeLanguage(e.key);
@@ -35,10 +42,11 @@ export default function LanguageSwitcher() {
       menu={{
         items: langOptions,
         onClick: changeLocale,
+        defaultSelectedKeys: [lang],
       }}
     >
       <Button icon={<Icon component={I18nSvg} />} type="link" style={{ color: 'rgba(0, 0, 0, 0.85)' }}>
-        {lang}
+        {langLabel}
       </Button>
     </Dropdown>
   );
