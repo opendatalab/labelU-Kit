6a1cc5a9bea634911da0f8e4b996c3625344ddb7
gary-Shen <grinson50@gmail.com>
2025-01-14 15:29:05 +0800

    fix(frontend): fix export error

--- apps/frontend/src/api/constant.ts
+++ apps/frontend/src/api/constant.ts
@@ -1,25 +1,29 @@
+import { i18n } from '@labelu/i18n';
+
 const COMMON_INIT_CODE = 30000;
 const USER_INIT_CODE = 40000;
 const TASK_INIT_CODE = 50000;
+const EXPORT_INIT_CODE = 60000;
 
 export const ErrorMessages = {
-  400: '参数有问题',
-  [COMMON_INIT_CODE]: 'sql错误',
-  [COMMON_INIT_CODE + 1]: '静止,没有权限',
-  [COMMON_INIT_CODE + 2]: '请求的验证错误',
-  [COMMON_INIT_CODE + 3]: '错误的请求',
-  [USER_INIT_CODE]: '无效的用户名或密码',
-  [USER_INIT_CODE + 1]: '用户已经存在',
-  [USER_INIT_CODE + 2]: '用户没发现',
-  [USER_INIT_CODE + 3]: '不能验证证书',
-  [USER_INIT_CODE + 4]: '没有授权',
-  [TASK_INIT_CODE]: '服务器内部错误',
-  [TASK_INIT_CODE + 1]: '任务已完成',
-  [TASK_INIT_CODE + 2]: '任务没发现',
-  [TASK_INIT_CODE + 1000]: '上传文件错误,保存失败',
-  [TASK_INIT_CODE + 1001]: '附件未找到',
-  [TASK_INIT_CODE + 1002]: '同名附件已存在',
-  [TASK_INIT_CODE + 5000]: '参数错误：after,before,pageNo,只能选一个，pageNo 可以说是0',
-  [TASK_INIT_CODE + 5001]: '没有此sample',
-  [TASK_INIT_CODE + 5003]: '预标注中的sample_name在数据库中已存在',
+  400: i18n.t('queryParamsError'),
+  [COMMON_INIT_CODE]: i18n.t('sqlError'),
+  [COMMON_INIT_CODE + 1]: i18n.t('forbidden'),
+  [COMMON_INIT_CODE + 2]: i18n.t('validationFailed'),
+  [COMMON_INIT_CODE + 3]: i18n.t('badRequest'),
+  [USER_INIT_CODE]: i18n.t('invalidUserOrPassword'),
+  [USER_INIT_CODE + 1]: i18n.t('userExists'),
+  [USER_INIT_CODE + 2]: i18n.t('userNotExists'),
+  [USER_INIT_CODE + 3]: i18n.t('certError'),
+  [USER_INIT_CODE + 4]: i18n.t('unauthorized'),
+  [TASK_INIT_CODE]: i18n.t('serverError'),
+  [TASK_INIT_CODE + 1]: i18n.t('taskIsDone'),
+  [TASK_INIT_CODE + 2]: i18n.t('taskNotFound'),
+  [TASK_INIT_CODE + 1000]: i18n.t('fileUploadFailed'),
+  [TASK_INIT_CODE + 1001]: i18n.t('fileNotFound'),
+  [TASK_INIT_CODE + 1002]: i18n.t('duplicatedFile'),
+  [TASK_INIT_CODE + 5000]: i18n.t('参数错误：after,before,pageNo,只能选一个，pageNo 可以说是0'),
+  [TASK_INIT_CODE + 5001]: i18n.t('noSample'),
+  [TASK_INIT_CODE + 5003]: i18n.t('sampleNameExists'),
+  [EXPORT_INIT_CODE + 1000]: i18n.t('noDataExport'),
 } as const;
