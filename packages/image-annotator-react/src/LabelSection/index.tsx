0f34a0b7495d7751b65db895af0d0172747f8503
max <max.email>
2025-04-23 12:24:46 +0800

    fix(image-annotator-react): 修复#654问题。1.多标签显示不完整bug 2.图标重叠bug

--- packages/image-annotator-react/src/LabelSection/index.tsx
+++ packages/image-annotator-react/src/LabelSection/index.tsx
@@ -118,8 +118,9 @@ const MoreAttribute = styled.div<{ visible: boolean }>`
   background-color: #fff;
   right: 0;
   display: flex;
+  flex-wrap: wrap;
   top: 100%;
-  z-index: 999;
+  z-index: 1010;
   padding: 0.5rem;
   box-shadow: 0px 3px 6px 0px rgb(0 0 0 / 21%);
   border-radius: 3px;
