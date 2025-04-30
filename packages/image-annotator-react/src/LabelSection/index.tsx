c05c49e3776d23467fd842ab501d7b4a2b0b1720
gary-Shen <grinson50@gmail.com>
2025-04-30 14:34:58 +0800

    fix(image-annotator-react): label section style

--- packages/image-annotator-react/src/LabelSection/index.tsx
+++ packages/image-annotator-react/src/LabelSection/index.tsx
@@ -119,7 +119,9 @@ const MoreAttribute = styled.div<{ visible: boolean }>`
   right: 0;
   display: flex;
   top: 100%;
-  z-index: 999;
+  max-width: 50vw;
+  overflow: auto;
+  z-index: 1002;
   padding: 0.5rem;
   box-shadow: 0px 3px 6px 0px rgb(0 0 0 / 21%);
   border-radius: 3px;
