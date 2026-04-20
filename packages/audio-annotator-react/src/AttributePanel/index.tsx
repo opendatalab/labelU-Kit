4cf8cf1bab7d793dcf649a41e5ad7e1843aa5cce
shlab <shenguanlin@pjlab.org.cn>
2026-04-20 17:57:34 +0800

    Revert "fix(audio-annotator-react): replace noneLabel to input label"
    This reverts commit 2c396ab1cfb9651a9df22deff49eca0509335abc.

--- packages/audio-annotator-react/src/AttributePanel/index.tsx
+++ packages/audio-annotator-react/src/AttributePanel/index.tsx
@@ -410,7 +410,7 @@ export function AttributePanel() {
     () =>
       Array.from(mediaAnnotationGroup).map(([label, _annotations]) => {
         const found = labelMapping[_annotations[0].type]?.[label] ?? preLabelMapping?.[_annotations[0].type]?.[label];
-        const labelText = found ? found?.key ?? label : label;
+        const labelText = found ? found?.key ?? t('noneLabel') : t('noneLabel');
 
         return {
           label: (
@@ -434,7 +434,7 @@ export function AttributePanel() {
                     active={item.id === selectedAnnotation?.id}
                     order={item.order}
                     annotation={item}
-                    labelText={labelOfAnnotation?.key ?? label}
+                    labelText={labelOfAnnotation?.key ?? t('noneLabel')}
                     color={labelOfAnnotation?.color ?? '#999'}
                   />
                 );
@@ -443,7 +443,7 @@ export function AttributePanel() {
           ),
         };
       }),
-    [mediaAnnotationGroup, labelMapping, preLabelMapping, selectedAnnotation?.id],
+    [t, mediaAnnotationGroup, labelMapping, preLabelMapping, selectedAnnotation?.id],
   );
 
   return (
